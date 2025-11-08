e1310bc258d9d6d8d6126cacc7de2b4c39d29696
PC User <pcuser@example.local>
2025-11-08 13:23:30 +0100

    add lint check pipeline

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,9 @@
 import { test, expect } from '@playwright/test';
 
+
+
+
+
 test('homepage has title and get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   await expect(page).toHaveTitle(/Playwright/);
