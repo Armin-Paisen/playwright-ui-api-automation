34c83d2b68c1fdd85b72c80acb6295a0fb4d3487
PC User <pcuser@example.local>
2025-11-08 13:44:19 +0100

    Fix test script

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-import { a } from '@playwright/test';
-
 test('homepage has title and get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   await expect(page).toHaveTitle(/Playwright/);
